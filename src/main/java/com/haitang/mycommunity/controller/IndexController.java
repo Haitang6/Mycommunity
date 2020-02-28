24c2ded7ba80e32fa8de0170cde29e4bf25a1507
fangfang <2242779007@qq.com>
2020-02-28 16:25:24 +0800

    delete useless files

--- src/main/java/com/haitang/mycommunity/controller/IndexController.java
+++ src/main/java/com/haitang/mycommunity/controller/IndexController.java
@@ -1,4 +1,5 @@
 package com.haitang.mycommunity.controller;
+
 import com.haitang.mycommunity.dto.QuestionDto;
 import com.haitang.mycommunity.schedule.HotTagCache;
 import com.haitang.mycommunity.service.QuestionService;
@@ -6,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -23,18 +22,18 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request, Model model,
-                        @RequestParam(name = "search",required = false)String search,
-                        @RequestParam(name = "tag",required = false)String tag) {
+                        @RequestParam(name = "search", required = false) String search,
+                        @RequestParam(name = "tag", required = false) String tag) {
 
-        List<QuestionDto> questionDtos =questionService.findAll(search,tag);
-        model.addAttribute("questionDtos",questionDtos);
+        List<QuestionDto> questionDtos = questionService.findAll(search, tag);
+        model.addAttribute("questionDtos", questionDtos);
         List<String> tags = hotTagCache.getHots();
-        model.addAttribute("tags",tags);
+        model.addAttribute("tags", tags);
         return "index";
     }
 
     @GetMapping("/test")
-    public String test(Model model){
+    public String test(Model model) {
         return "test";
     }
 
--- src/main/java/com/haitang/mycommunity/controller/MyselfController.java
+++ src/main/java/com/haitang/mycommunity/controller/MyselfController.java
@@ -1,10 +1,7 @@
 package com.haitang.mycommunity.controller;
+
 import com.haitang.mycommunity.dto.NotificationDto;
 import com.haitang.mycommunity.dto.QuestionDto;
-import com.haitang.mycommunity.mapper.NotificationMapper;
-import com.haitang.mycommunity.mapper.UserMapper;
-import com.haitang.mycommunity.model.Notification;
-import com.haitang.mycommunity.model.Question;
 import com.haitang.mycommunity.model.User;
 import com.haitang.mycommunity.service.NotificationService;
 import com.haitang.mycommunity.service.QuestionService;
@@ -13,9 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -30,21 +24,21 @@ public class MyselfController {
     @GetMapping("/myself/{action}")
     public String myself(@PathVariable(name = "action") String action,
                          HttpServletRequest request,
-                         Model model){
-        User user=(User)request.getSession().getAttribute("user");
-        if(user == null){
+                         Model model) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
             return "redirect:/";
         }
-        if ("question".equals(action)){
+        if ("question".equals(action)) {
             List<QuestionDto> questionDtos = questionService.findAllByUserid(user.getId());
-            model.addAttribute("questionDtos",questionDtos);
-            model.addAttribute("title","我的问题");
-            model.addAttribute("titleName","question");
-        }else if ("replies".equals(action)){
+            model.addAttribute("questionDtos", questionDtos);
+            model.addAttribute("title", "我的问题");
+            model.addAttribute("titleName", "question");
+        } else if ("replies".equals(action)) {
             List<NotificationDto> notificationDtos = notificationService.list(user.getId());
-            model.addAttribute("title","我的回复");
-            model.addAttribute("titleName","replies");
-            model.addAttribute("notificationDtos",notificationDtos);
+            model.addAttribute("title", "我的回复");
+            model.addAttribute("titleName", "replies");
+            model.addAttribute("notificationDtos", notificationDtos);
             return "myself";
         }
         return "myself";
